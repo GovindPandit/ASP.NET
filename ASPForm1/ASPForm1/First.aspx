0c76a6856e4a9143bad527595c7aeb27b2e3b3f0
Govind Pandit <govindpanditniit@gmail.com>
2021-07-09 23:34:39 +0530

    Add project files.

---
+++ ASPForm1/ASPForm1/First.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="First.aspx.cs" Inherits="ASPForm1.First" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="Number" runat="server"></asp:TextBox>
+            <asp:Button ID="Click" runat="server" Text="Click" OnClick="OnButtonClicked"/>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASPForm1/ASPForm1/First.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPForm1
+{
+    public partial class First : System.Web.UI.Page
+    {
+        int count = 1;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                Number.Text = "0";
+            }
+        }
+
+        protected void OnButtonClicked(object sender, EventArgs e)
+        {
+            if(ViewState["Click"]!=null)
+            {
+                count = (int)ViewState["Click"] + 1;
+            }
+
+            ViewState["Click"] = count;
+            Number.Text = count.ToString();
+        }
+    }
+}
